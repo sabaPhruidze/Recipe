aa090263cb8a084720089039e74942e7d25b0014
Saba Phruidze <sabaphruidze01@gmail.com>
2023-09-24 20:25:29 +0400

    Update

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native";
+import styled from "styled-components";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to sta rt working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <StatusBar style="light" />
+      <RecipeBackground source={require("./assets/main.jpg")} />
+      <SafeAreaView>
+        <MenuBar>
+          <Back>
+            <AntDesign name="arrowleft" size={24} color="#000" />
+            <Text>Ingredients</Text>
+          </Back>
+        </MenuBar>
+      </SafeAreaView>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
+
+const RecipeBackground = styled.Image`
+  width: 100%;
+  height: 100px;
+`;
+
+const Text = styled.Text`
+  /* color: ${(props) => (props.dark ? "#000" : "#fff")};
+  font-family: "AvenirNext-Regular"; */
+`;
+
+const MenuBar = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 16px;
+`;
+
+const Back = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
